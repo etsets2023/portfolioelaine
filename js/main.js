a86e22080b08fb425cc02ad8aad89ab992585304
ElaineTavares <etsets.tavares@gmail.com>
2023-12-12 20:47:10 -0300

    menu hamb

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function changeMenu() {
+    if (document.getElementById('navbar').style.display == 'none') {
+        document.getElementById('navbar').style.display = 'flex'
+    } else {
+        document.getElementById('navbar').style.display = 'none'
+    }
+}
